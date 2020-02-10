230175dab56a3adbfb80bb7b1e46ad85ad60ffdf
John Fletcher <john.fletcher@codecentric.de>
2020-02-10 18:12:26 +0100

    Test implementation for issue 93, missing actual AOP part.

---
+++ chaos-monkey-spring-boot/src/main/java/de/codecentric/spring/boot/chaos/monkey/assaults/ResponseModifyingRequestInterceptor.java
@@ -0,0 +1,48 @@
+package de.codecentric.spring.boot.chaos.monkey.assaults;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.AbstractClientHttpResponse;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+public class ResponseModifyingRequestInterceptor implements ClientHttpRequestInterceptor {
+
+  public ClientHttpResponse intercept(
+      HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+    return new InternalServerErrorHttpResponse();
+  }
+
+  class InternalServerErrorHttpResponse extends AbstractClientHttpResponse {
+    @Override
+    public HttpHeaders getHeaders() {
+      return new HttpHeaders();
+    }
+
+    @Override
+    public InputStream getBody() throws IOException {
+      return new ByteArrayInputStream(
+          "{\"error\": \"Chaos Monkey for Spring Boot generated failure\"}"
+              .getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public int getRawStatusCode() throws IOException {
+      return 500;
+    }
+
+    @Override
+    public String getStatusText() throws IOException {
+      return HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
+    }
+
+    @Override
+    public void close() {}
+  }
+}
---
+++ chaos-monkey-spring-boot/src/test/java/de/codecentric/spring/boot/chaos/monkey/assaults/RestTemplateAssaultTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.codecentric.spring.boot.chaos.monkey.assaults;
+
+import static org.mockito.Mockito.verify;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/** @author Benjamin Wilms */
+@ExtendWith(MockitoExtension.class)
+class RestTemplateAssaultTest {
+
+  private RestTemplate restTemplate = new RestTemplate();
+
+  @Test
+  void assaultRestTemplateCallWithFailure() {
+    restTemplate.setInterceptors(
+        Collections.singletonList(new ResponseModifyingRequestInterceptor()));
+    Assertions.assertThatThrownBy(
+            () ->
+                restTemplate.getForEntity(
+                    "https://jsonplaceholder.typicode.com/todos/1", String.class))
+        .isInstanceOf(RestClientException.class)
+        .hasMessage("500 Internal Server Error");
+  }
+
+  @Test
+  void assaultRestTemplateCallWithLatency() {
+    restTemplate.setInterceptors(
+        Collections.singletonList(new LatencyInducingRequestInterceptor()));
+    Instant start = Instant.now();
+    restTemplate.getForEntity("https://jsonplaceholder.typicode.com/todos/1", String.class);
+    Instant end = Instant.now();
+
+    Duration d = Duration.between(start, end);
+
+    Assertions.assertThat(d.toMillis()).isCloseTo(2000L, Offset.offset(500L));
+  }
+}
